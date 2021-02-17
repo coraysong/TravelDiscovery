631e3e9241843bd79b9ed799f5b4d54cd5bc47ac
Coray <coray@itwakes.me>
2021-02-17 14:26:46 +0800

    01.First commit

---
+++ TravelDiscoveryLBTA/TravelDiscoveryLBTA/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  TravelDiscoveryLBTA
+//
+//  Created by Coray on 17/2/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView{
+            ScrollView{
+                DiscoverCategoriesView()
+                
+            }.navigationTitle("Discover")
+            
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct DiscoverCategoriesView: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators:false){
+            HStack(spacing: 8){
+                ForEach(0..<7, id: \.self) { num in
+                    VStack(spacing: 4){
+                        Spacer()
+                            .frame(width: 50, height: 50)
+                            .background(Color.gray)
+                            .cornerRadius(.infinity)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+                        Text("Art")
+                            .font(.system(size: 12, weight: .semibold))
+                    }
+                    
+                }
+                
+            }
+            .padding(.horizontal)
+        }
+    }
+}
