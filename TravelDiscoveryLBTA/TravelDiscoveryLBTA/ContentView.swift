15c6426c3c960bcb2c6408c6db90baa1923da116
Coray <coray@itwakes.me>
2021-02-19 13:45:30 +0800

    03.complete destinations

--- TravelDiscoveryLBTA/TravelDiscoveryLBTA/ContentView.swift
+++ TravelDiscoveryLBTA/TravelDiscoveryLBTA/ContentView.swift
@@ -22,7 +22,21 @@ struct ContentView: View {
     }
 }
 
+struct Destination: Hashable {
+    let name, country, imageName: String
+    
+}
+
+
 struct PopularDestinationsView: View {
+    
+    let destinations:[Destination] = [
+        .init(name: "Paris", country: "France", imageName: "eiffel_tower"),
+        .init(name: "Tokyo", country: "Japan", imageName: "japan"),
+        .init(name: "New York", country: "US", imageName: "new_york")
+    ]
+    
+    
     var body: some View {
         VStack {
             HStack{
@@ -36,10 +50,27 @@ struct PopularDestinationsView: View {
             
             ScrollView(.horizontal){
                 HStack(spacing: 8.0){
-                    ForEach(0..<5, id: \.self) { num in
-                        Spacer()
-                            .frame(width: 125, height: 150)
-                            .background(Color.gray)
+                    ForEach(destinations, id: \.self) { destination in
+                        VStack(alignment: .leading){
+                            Image(destination.imageName)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 125, height: 125)
+                                .cornerRadius(5)
+                                .padding(.horizontal, 6)
+                                .padding(.vertical, 6)
+                            
+                            Text(destination.name)
+                                .font(.system(size: 12, weight: .semibold))
+                                .padding(.horizontal, 12)
+                            Text(destination.country)
+                                .font(.system(size: 12, weight: .semibold))
+                                .padding(.horizontal, 12)
+                                .padding(.bottom, 8)
+                                .foregroundColor(.gray)
+                        }
+
+                            .background(Color(.init(white: 0.9, alpha: 1)))
                             .cornerRadius(5)
                             .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
                             .padding(.bottom)
@@ -105,6 +136,7 @@ struct TreadingCreatorsView: View {
         }
     }
 }
+
 
 struct Category: Hashable {
     let name, imageName: String
