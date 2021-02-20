62b0f607320dc05b63cdcdbe3f953c1b4f695d68
Coray <coray@itwakes.me>
2021-02-20 15:20:43 +0800

    Add  zstack

--- TravelDiscoveryLBTA/TravelDiscoveryLBTA/ContentView.swift
+++ TravelDiscoveryLBTA/TravelDiscoveryLBTA/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [
+            .foregroundColor: UIColor.white
+        ]
+    }
+    
     var body: some View {
         NavigationView{
-            ScrollView{
-                DiscoverCategoriesView()
-                PopularDestinationsView()
-                PopularRestaurantsView()
-                TreadingCreatorsView()
-            }.navigationTitle("Discover")
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.9882131219, green: 0.6823856831, blue: 0.2509839535, alpha: 1)), Color(#colorLiteral(red: 0.996080339, green: 0.446325405, blue: 0.2697934847, alpha: 1))]), startPoint: .top, endPoint: .center)
+                    .ignoresSafeArea()
+                
+                Color(.init(white: 0.95, alpha: 1)).offset(y: 400)
+                ScrollView{
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                        Text("Where do you want to go?")
+                        Spacer()
+                    }.font(.system(size: 14, weight: .semibold))
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color(.init(white: 1, alpha: 0.3)))
+                    .cornerRadius(10)
+                    .padding(16)
+                    DiscoverCategoriesView()
+                    VStack{
+                        PopularDestinationsView()
+                        PopularRestaurantsView()
+                        TreadingCreatorsView()
+                    }.background(Color(.init(white: 0.95, alpha: 1)))
+                    .cornerRadius(16)
+                    .padding(.top, 32)
+                    
+                }.navigationTitle("Discover")
+            }
             
         }
         
